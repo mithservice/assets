3e0341c4b12aa9922e7631218b9e4c1eb5ae511b
mithservice <124500929+mithservice@users.noreply.github.com>
2023-05-31 14:47:58 +0700

    Update RollingCurl.php

--- RollingCurl.php
+++ RollingCurl.php
@@ -9,8 +9,8 @@ class RollingCurl
     protected $options = array(
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_FOLLOWLOCATION => 1,
-        CURLOPT_CONNECTTIMEOUT => 30,
-        CURLOPT_TIMEOUT        => 30,
+        CURLOPT_CONNECTTIMEOUT => 60,
+        CURLOPT_TIMEOUT        => 60,
         CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
     );
    protected $multicurlOptions = array();
